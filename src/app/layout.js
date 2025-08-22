63d31e854ae23cdb6a7eb4c60a64292a843a393a
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-08-22 11:06:08 +0600

    feat: basic NextAuth logi implemented

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/NavBar/Navbar";
+import NextAuthSessionProvider from "@/providers/NextAuthSessionProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,18 +24,20 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="min-h-screen w-full relative">
-          {/* Cosmic Noise */}
-          <div
-            className="absolute inset-0 -z-10"
-            style={{
-              background: "radial-gradient(circle at 20% 20%, rgba(255,255,255,0.08) 0%, transparent 40%), radial-gradient(circle at 80% 30%, rgba(255,255,255,0.05) 0%, transparent 40%), linear-gradient(120deg, #0f0e17 0%, #1a1b26 100%)",
-              backgroundAttachment: 'fixed'
-            }}
-          />
-          <Navbar />
-          {children}
-        </div>
+        <NextAuthSessionProvider>
+          <div className="min-h-screen w-full relative">
+            {/* Cosmic Noise */}
+            <div
+              className="absolute inset-0 -z-10"
+              style={{
+                background: "radial-gradient(circle at 20% 20%, rgba(255,255,255,0.08) 0%, transparent 40%), radial-gradient(circle at 80% 30%, rgba(255,255,255,0.05) 0%, transparent 40%), linear-gradient(120deg, #0f0e17 0%, #1a1b26 100%)",
+                backgroundAttachment: 'fixed'
+              }}
+            />
+            <Navbar />
+            {children}
+          </div>
+        </NextAuthSessionProvider>
       </body>
     </html>
   );
