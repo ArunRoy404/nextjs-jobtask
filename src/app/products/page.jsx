697505c7c0170afb298b81f97c48eace5917298e
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-08-22 13:24:53 +0600

    refactor: fetch link updated

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,9 +1,9 @@
 import { Star, ShoppingCart, Heart, Eye } from 'lucide-react';
 
 const page = async () => {
-const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
-const data = await res.json();
-    const products = await data.data
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+    const data = await res.json();
+    const products = await data?.data
     return (
         <div className='pt-40 py-30'>
             <div className='container px-4 sm:px-6 lg:px-8 mx-auto'>
